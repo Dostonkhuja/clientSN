e18e0ea49e626dc1ef0a80b7e5f835b6edee971f
Dostonkhuja <77501580+Dostonkhuja@users.noreply.github.com>
2025-08-04 12:59:13 +0500

    Update SignUp.jsx
    corrected UI

--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -1,47 +1,89 @@
 import * as React from 'react';
-import {useEffect} from 'react';
-import {useDispatch, useSelector} from "react-redux";
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import { sendSIgnIn } from '../../State-management/SignInSlice';
+import SignUpForm from './SignUpForm';
+
+import {
+    Avatar,
+    Box,
+    Container,
+    CssBaseline,
+    Typography,
+    Paper,
+} from '@mui/material';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import {Avatar, Box, CssBaseline, Grid, Link, Typography} from '@mui/material'
-import {Redirect} from "react-router-dom";
-import {sendSIgnIn} from "../../State-management/SignInSlice";
-import SignUpForm from "./SignUpForm";
 
 const SignUp = React.memo(() => {
-    const dispatch = useDispatch()
-    const errorMessage = useSelector(state => state.signUp.errorMessage)
-    const user = useSelector(state => state.signUp.user)
-    const signInData = useSelector(state => state.signUp.signInData)
-    const isAuth = useSelector(state => state.signIn.isAuth)
+    const dispatch = useDispatch();
+
+    const errorMessage = useSelector(state => state.signUp.errorMessage);
+    const user = useSelector(state => state.signUp.user);
+    const signInData = useSelector(state => state.signUp.signInData);
+    const isAuth = useSelector(state => state.signIn.isAuth);
 
     useEffect(() => {
         if (!isAuth && user && signInData) {
-            dispatch(sendSIgnIn(signInData))
+            dispatch(sendSIgnIn(signInData));
         }
-    }, [user])
+    }, [user]);
 
-    if (isAuth){
-        return <Redirect to='/profile'/>
+    if (isAuth) {
+        return <Redirect to="/profile" />;
     }
 
-    return <Grid item xs={12} component="main" maxWidth="xs">
-                <CssBaseline/>
-                <Box sx={{pl:25,marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                    <Avatar  sx={{m: 1, backgroundColor: '#1976d2'}}>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">Sign up</Typography>
-                  <SignUpForm errorMessage={errorMessage}/>
-                </Box>
-                <Typography variant="body2" color="text.secondary" align="center" sx={{mt: 8, mb: 4,pl:20}}>
-                    {'Copyright © '}
-                    <Link color="inherit" href="/signIn">
-                        Doston Sheraliyev social-network app
-                    </Link>{' '}
-                    {new Date().getFullYear()}
-                    {'.'}
-                </Typography>
-            </Grid>
-})
+    return (
+        <Container component="main" maxWidth="md">
+            <CssBaseline />
 
-export default SignUp
+            {/* Sayt nomi */}
+            <Box sx={{ mt: 8, textAlign: 'center' }}>
+                <Typography
+                    variant="h4"
+                    fontWeight={700}
+                    sx={{
+                        color: '#1976d2',
+                        fontSize: { xs: '1.8rem', sm: '2.4rem' },
+                        mb: 6, // nomdan keyin bo'sh joy
+                    }}
+                >
+                    The Beatles network
+                </Typography>
+            </Box>
+
+            {/* Forma konteyneri */}
+            <Box
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                minHeight="60vh"
+                px={2}
+                sx={{ mb: 10 }} // pastga bo'sh joy
+            >
+                <Paper
+                    elevation={4}
+                    sx={{
+                        p: { xs: 3, sm: 4 },
+                        width: '100%',
+                        maxWidth: 400,
+                        borderRadius: 2,
+                    }}
+                >
+                    <Box display="flex" flexDirection="column" alignItems="center">
+                        <Avatar sx={{ m: 1, bgcolor: '#1976d2' }}>
+                            <LockOutlinedIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5" sx={{ mb: 2 }}>
+                            Sign up
+                        </Typography>
+
+                        <SignUpForm errorMessage={errorMessage} />
+                    </Box>
+                </Paper>
+            </Box>
+        </Container>
+    );
+});
+
+export default SignUp;
